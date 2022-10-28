79ce414dc488f9da00b0fd27814cfad1310f6c09
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-10-28 10:57:33 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,8 +1,8 @@
 #providers######
 
 provider "aws" {
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
     region = var.aws_region
     
 }
