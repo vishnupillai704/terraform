7bcc62b44882b2e0eec23848844e268e1c549547
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-10-28 12:58:58 +0530

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -1,6 +1,6 @@
 ###AWS INSTANCE########
 resource "aws_instance" "nginx1" {
-    ami = nonsensitive(data.aws_ssm_parameter.ami.value)
+    ami = data.aws_ssm_parameter.ami.value
     instance_type = var.instance_type
     subnet_id = aws_subnet.subnet1.id
     vpc_security_group_ids = [aws_security_group.nginx_sg.id]
@@ -8,7 +8,7 @@ tags = local.common_tags
   
 }
 resource "aws_instance" "nginx2" {
-    ami = nonsensitive(data.aws_ssm_parameter.ami.value)
+    ami = data.aws_ssm_parameter.ami.value
     instance_type = var.instance_type
     subnet_id = aws_subnet.subnet2.id
     vpc_security_group_ids = [aws_security_group.nginx_sg.id]
