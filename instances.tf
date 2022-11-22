e8a3c0556406cf115bf6df5cba6019f99c3e53d8
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-11-22 14:35:18 +0530

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -6,7 +6,11 @@ resource "aws_instance" "nginx1" {
     vpc_security_group_ids = [aws_security_group.nginx_sg.id]
     user_data = <<EOF
         #! /bin/bash
+	sudo yum update -y
+        sudo yum install -y docker
         sudo service docker start
+        sudo usermod -a -G docker ec2-user
+	sudo docker pull vishnu981824/app1
         sudo docker run vishnu981824/app1
 	EOF
         tags = local.common_tags 
@@ -17,6 +21,14 @@ resource "aws_instance" "nginx2" {
     instance_type = var.instance_type
     subnet_id = aws_subnet.subnet2.id
     vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+     user_data = <<EOF
+        #! /bin/bash
+	sudo yum update -y
+        sudo yum install -y docker
+        sudo service docker start
+        sudo usermod -a -G docker ec2-user
+	sudo docker pull vishnu981824/app1
+        sudo docker run vishnu981824/app1
 
 tags = local.common_tags 
   
