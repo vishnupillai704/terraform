c25976ef764ebeb139fbe423e0790f6bead52d46
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-11-22 11:22:07 +0530

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -5,11 +5,11 @@ resource "aws_instance" "nginx1" {
     subnet_id = aws_subnet.subnet1.id
     vpc_security_group_ids = [aws_security_group.nginx_sg.id]
     user_data = << EOF
-		#! /bin/bash
+        #! /bin/bash
         sudo service docker start
         sudo docker run vishnu981824/app1
 	EOF
-tags = local.common_tags 
+        tags = local.common_tags 
   
 }
 resource "aws_instance" "nginx2" {
