d973fc9d4e541847e94d0f299f96a90b7c6dc206
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-11-22 15:00:32 +0530

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "nginx1" {
     instance_type = var.instance_type
     subnet_id = aws_subnet.subnet1.id
     vpc_security_group_ids = [aws_security_group.nginx_sg.id]
-    key_name = "kk"
+    key_name = "pk"
     user_data = <<EOF
         #! /bin/bash
 	sudo yum update -y
@@ -22,7 +22,7 @@ resource "aws_instance" "nginx2" {
     instance_type = var.instance_type
     subnet_id = aws_subnet.subnet2.id
     vpc_security_group_ids = [aws_security_group.nginx_sg.id]
-    key_name = "kk"
+    key_name = "pk"
     user_data = <<EOF
         #! /bin/bash
 	sudo yum update -y
